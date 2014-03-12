1a88d69df6929a4bcb89bfae1a8253aca948aaf2
fear-sfx <tmanoilov@abv.bg>
2014-03-12 10:43:01 +0200

    blah
    just see it

--- src/com/ballstoyou/servlets/PostMessage.java
+++ src/com/ballstoyou/servlets/PostMessage.java
@@ -1,9 +1,6 @@
 package com.ballstoyou.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Date;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,14 +10,26 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class PostMessage
  **/
-@WebServlet("/PostMessage")
-public class PostMessageServlet extends HttpServlet {
-  private static final long serialVersionUID = 187436524897235619654937851L;
+//@WebServlet("/PostMessage/*")
+public class PostMessage extends HttpServlet {
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1183188047248672817L;
 
   /**
    * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
    **/
-  protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//  protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//	  
+//  }
+  
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String text = "some text";
 
+      response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
+      response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+      response.getWriter().write(text);       // Write response body.
   }
 }
