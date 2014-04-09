7be7f9cedabdac4f0ed59febea0d72f45ef412c1
fear-sfx <tmanoilov@abv.bg>
2014-04-09 12:34:18 +0300

    byqqqq

--- src/com/ballstoyou/servlets/PostMessage.java
+++ src/com/ballstoyou/servlets/PostMessage.java
@@ -1,8 +1,9 @@
 package com.ballstoyou.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -25,11 +26,18 @@ public class PostMessage extends HttpServlet {
 //  }
   
   @Override
-  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      String text = "some text";
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      String msg = request.getParameter("message");
+      
+//      PrintWriter out = response.getWriter();  
+//      response.setContentType("text/html");  
+//      out.println("<script type=\"text/javascript\">");  
+//      out.println("alert('"+msg+"');");  
+//      out.println("logout();");
+//      out.println("</script>");
 
       response.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
       response.setCharacterEncoding("UTF-8"); // You want world domination, huh?
-      response.getWriter().write(text);       // Write response body.
-  }
+      response.getWriter().write(msg);       // Write response body.
+  }	
 }
